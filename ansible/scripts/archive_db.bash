b370640e729d10375620a5fd0dc3bb393f5156f5
Matthew Pearson <mpearson@pih.org>
2022-12-22 13:07:30 -0800

    OBS-1360 Build playbooks for configuring OB hosts.
    Includes fixes for the following:
    - OBS-1347 Allow Bamboo access.
    - OBS-1373 Configure GPars/Quartz pool sizes and improve monitoring.
    - OBS-1375 Add config data from existing obnav instance.
    - OBS-1376 Add playbook to reset dev database(s).
    - OBS-1377 Declare Quartz config in a way Grails 3 builds can access.
    - OBS-1382 Support separate web/db servers in prd and stg.

---
+++ ansible/scripts/archive_db.bash
@@ -0,0 +1,144 @@
+#!/bin/bash
+set -euo pipefail
+
+usage()
+{
+    echo >&2 "$0 - archive an OpenBoxes database to a file"
+    echo >&2 ''
+    echo >&2 "Usage: $0 [-sP] [-d <db_name>] [-o <archive_file> ] [-p <mysql_path> ] [-u <db_user> ]"
+    echo >&2 ''
+    echo >&2 'Options:'
+    echo >&2 '  -s use sudo when accessing db server (if using auth_socket for root)'
+    echo >&2 '  -P Copy product_demand tables (will fail if refreshProductDemandData is running on remote)'
+    echo >&2 '  -d <db_name> -- name of database to archive (default: "openboxes")'
+    echo >&2 '  -o <archive_file> -- name of archive file (default: "<db_name>.tgz")'
+    echo >&2 '  -p <mysql_path> -- path under which mysql is installed'
+    echo >&2 '  -u <db_user> -- name of database user (default: "root")'
+
+    [ "$@" ] && exit "$@" || exit 2
+}
+
+archive_file=
+db_name='openboxes'
+db_user='root'
+local_sudo=
+mysql_path=/usr/bin:/usr/local/mysql/bin
+declare -a ignored_tables
+
+while getopts 'd:ho:p:r:su:P' o
+do
+    case "$o" in
+    d)
+        [ "$OPTARG" ] || usage
+        db_name="$OPTARG"
+        ;;
+    h)
+        usage 0
+        ;;
+    o)
+        [ "$OPTARG" ] || usage
+        archive_file="$OPTARG"
+        ;;
+    p)
+        [ "$OPTARG" ] || usage
+        mysql_path="$OPTARG:$mysql_path"
+        ;;
+    s)
+        local_sudo=sudo
+        ;;
+    u)
+        [ "$OPTARG" ] || usage
+        db_user="$OPTARG"
+        ;;
+    P)
+        do_copy_product_demand=1
+        ;;
+    *)
+        usage
+        ;;
+    esac
+done
+shift $((OPTIND-1))
+
+set +u
+[ "$#" -eq 0 ] || usage
+if [ ! "$DB_PASSWORD" ]
+then
+    echo >&2 'please set the DB_PASSWORD environment variable'
+    exit 1
+fi
+set -u
+
+sql_basename="$db_name"
+[ "$archive_file" ] || archive_file="${sql_basename}.tgz"
+export PATH=$PATH:$mysql_path
+
+scratch_dir=$(mktemp -d)
+delete_scratch_dir() {
+    rm -rf "$scratch_dir"
+}
+trap delete_scratch_dir EXIT
+cd "$scratch_dir"
+
+$local_sudo true
+
+echo -n "Counting products in database \`$db_name\` ..."
+product_cnt=$($local_sudo mysql -u "$db_user" -p"$DB_PASSWORD" "$db_name" -Nse 'select count(id) from product;')
+echo " $product_cnt"
+
+if [ ! "${do_copy_product_demand:-}" ]
+then
+    #
+    # ReportService.refreshProductDemandData() drops tables while it works,
+    # which can generate "ERROR 1146 (42S02) at line XXXX: Table doesn't exist"
+    # errors and make dependent views un-reconstructable.
+    #
+    declare -a nocreate_tables=(
+        'product_demand_details'
+        'product_demand_details_tmp'
+        'product_expiry_summary'  # this view is invalid when product_demand_details is dropped
+    )
+    echo 'Skipping tables overwritten by ReportService.refreshProductDemandData() ...'
+    for it in "${nocreate_tables[@]}"
+    do
+        echo " - will not create \`$it\`"
+        ignored_tables+=("--ignore-table=$db_name.$it")
+    done
+fi
+
+echo "Exporting schema (ignoring ${#ignored_tables[@]} tables) from database \`$db_name\` ..."
+$local_sudo mysqldump -u "$db_user" -p"$DB_PASSWORD" \
+    --opt --allow-keywords --events --no-data --routines --single-transaction \
+    "${ignored_tables[@]:-}" "$db_name" \
+    > "${sql_basename}-schema.sql"
+
+# quick sanity check -- preceding command has some tricky shell expressions
+if [ ! "${do_copy_product_demand:-}" ]
+then
+    instance_count=$(grep -c "${nocreate_tables[0]}" "${sql_basename}-schema.sql" || :)
+    if [ "$instance_count" != "0" ]
+    then
+        echo >&2 "failed to filter tables as expected; at least ${instance_count} product-demand references remain"
+        exit 5
+    fi
+fi
+
+echo "Inspecting tables in database \`$db_name\` ..."
+nocopy_tables=$($local_sudo mysql -u "$db_user" -p"$DB_PASSWORD" -Nse 'select table_name from information_schema.tables where (table_schema like "'"$db_name"'") and (table_name like "%_dimension" or table_name like "%_fact" or table_name like "%_snapshot");')
+
+for it in $nocopy_tables
+do
+    echo " - will not copy \`$it\`"
+    ignored_tables+=("--ignore-table=$db_name.$it")
+done
+
+echo "Exporting data (ignoring ${#ignored_tables[@]} tables) from database \`$db_name\` ..."
+$local_sudo mysqldump -u "$db_user" -p"$DB_PASSWORD" \
+    --opt --allow-keywords --no-create-info --single-transaction \
+    "${ignored_tables[@]:-}" "$db_name" \
+    > "${sql_basename}-data.sql"
+
+echo "Compressing data to $archive_file ..."
+tar acvf "$OLDPWD/$archive_file" "${sql_basename}-schema.sql" "${sql_basename}-data.sql"
+
+echo 'Done'
---
+++ ansible/scripts/restore_db.bash
@@ -0,0 +1,135 @@
+#!/bin/bash
+set -euo pipefail
+
+usage()
+{
+    echo >&2 "$0 - restore an OpenBoxes database from a file"
+    echo >&2 ''
+    echo >&2 "Usage: $0 [-fs] [-d <db_name>] [-i <archive_file> ] [-p <mysql_path> ] [-u <db_user> ]"
+    echo >&2 ''
+    echo >&2 'Options:'
+    echo >&2 '  -f Completely delete <db_name>, if it exists, before restoring -- use with care!'
+    echo >&2 '  -s use sudo when accessing db server (if using auth_socket for root)'
+    echo >&2 '  -d <db_name> -- name under which database should be stored (default: "openboxes")'
+    echo >&2 '  -i <archive_file> -- name of archive file to restore (default: "<db_name>.tgz")'
+    echo >&2 '  -p <mysql_path> -- path under which mysql is installed'
+    echo >&2 '  -u <db_user> -- name of database user (default: "root")'
+
+    [ "$@" ] && exit "$@" || exit 2
+}
+
+archive_file=
+db_name='openboxes'
+db_user='root'
+local_sudo=
+mysql_path=/usr/bin:/usr/local/mysql/bin
+
+while getopts 'd:fhi:p:r:su:' o
+do
+    case "$o" in
+    d)
+        [ "$OPTARG" ] || usage
+        db_name="$OPTARG"
+        ;;
+    f)
+        do_clobber=1
+        ;;
+    h)
+        usage 0
+        ;;
+    i)
+        [ "$OPTARG" ] || usage
+        archive_file="$OPTARG"
+        ;;
+    p)
+        [ "$OPTARG" ] || usage
+        mysql_path="$OPTARG:$mysql_path"
+        ;;
+    s)
+        local_sudo=sudo
+        ;;
+    u)
+        [ "$OPTARG" ] || usage
+        db_user="$OPTARG"
+        ;;
+    *)
+        usage
+        ;;
+    esac
+done
+shift $((OPTIND-1))
+
+set +u
+[ "$#" -eq 0 ] || usage
+if [ ! "$DB_PASSWORD" ]
+then
+    echo >&2 'please set the DB_PASSWORD environment variable'
+    exit 1
+fi
+set -u
+
+sql_basename="$db_name"
+[ "$archive_file" ] || archive_file="${sql_basename}.tgz"
+export PATH=$PATH:$mysql_path
+
+scratch_dir=$(mktemp -d)
+delete_scratch_dir() {
+    rm -rf "$scratch_dir"
+}
+trap delete_scratch_dir EXIT
+cd "$scratch_dir"
+$local_sudo true
+
+echo "Extracting archived database from $archive_file ..."
+tar xvf "$OLDPWD/$archive_file"
+
+database_exists=$($local_sudo mysql -u "$db_user" -p"$DB_PASSWORD" -Nse "select count(schema_name) from information_schema.schemata where schema_name = '$db_name';")
+if [ "${do_clobber:-}" ]
+then
+    echo "Clobbering database \`$db_name\` ..."
+    $local_sudo mysql -u "$db_user" -p"$DB_PASSWORD" -e "drop database if exists \`$db_name\`;"
+elif [ "$database_exists" -ne 0 ]
+then
+    echo >&2 "Database \`$db_name\` exists and -f was not set!"
+    exit 2
+fi
+
+echo "Initializing database \`$db_name\` ..."
+$local_sudo mysql -u "$db_user" -p"$DB_PASSWORD" -e "create database \`$db_name\` default charset utf8; grant all on \`$db_name\`.* to 'openboxes'@'localhost' identified by 'openboxes';"
+$local_sudo mysql -u "$db_user" -p"$DB_PASSWORD" "$db_name" -e 'select 1' > /dev/null
+
+echo "Inserting schema into database \`$db_name\` ..."
+original_db_name=$(basename ./*-schema.sql -schema.sql)
+cat "${original_db_name}-schema.sql" \
+    | sed "/SQL SECURITY DEFINER/! s/\`$original_db_name\`/\`$db_name\`/g" \
+    | $local_sudo mysql -u "$db_user" -p"$DB_PASSWORD" "$db_name"
+
+#
+# Prevent "ERROR 2006 (HY000): MySQL server has gone away" by temporarily
+# increasing max_allowed_packet, if needed. MySQL 5.7's default value of 4M
+# may not be enough to consistently copy a production OB database, but if the
+# db server has already increased it, we don't need, or want, to change it.
+#
+# see also https://stackoverflow.com/questions/10474922/error-2006-hy000-mysql-server-has-gone-away
+#
+curr_max_packet=$($local_sudo mysql -u "$db_user" -p"$DB_PASSWORD" -Nse 'select @@max_allowed_packet;')
+if [ "$curr_max_packet" -lt 16777216 ]
+then
+    echo 'Temporarily increasing max_allowed_packet to 16M ...'
+    $local_sudo mysql -u -u "$db_user" -p"$DB_PASSWORD" -e 'set global max_allowed_packet=16*1024*1024;'
+fi
+
+echo "Inserting data into database \`$db_name\` (this may take several minutes) ..."
+$local_sudo mysql -u "$db_user" -p"$DB_PASSWORD" "$db_name" < "${original_db_name}-data.sql"
+
+if [ "$curr_max_packet" -lt 16777216 ]
+then
+    echo 'Restoring previous max_allowed_packet ...'
+    $local_sudo mysql -u "$db_user" -p"$DB_PASSWORD" -e "set global max_allowed_packet=$curr_max_packet;"
+fi
+
+echo -n "Counting products in database \`$db_name\` ..."
+product_cnt=$($local_sudo mysql -u "$db_user" -p"$DB_PASSWORD" "$db_name" -Nse 'select count(id) from product;')
+echo " $product_cnt"
+
+echo 'Done'
