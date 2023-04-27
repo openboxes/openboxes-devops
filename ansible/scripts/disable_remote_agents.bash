492664b23d6950c0f605b409d0fa0d975288c3ac
Matthew Pearson <mpearson@pih.org>
2023-04-27 14:04:09 -0700

    Remove all tabs from source code

--- ansible/scripts/disable_remote_agents.bash
+++ ansible/scripts/disable_remote_agents.bash
@@ -52,33 +52,33 @@ fi
 set -u
 
 remote_agents=$(
-	curl -X GET -su "${bamboo_user}:${BAMBOO_PASSWORD}" \
-	"${bamboo_url}/rest/api/latest/agent/remote" \
-	| jq -r '.[] | .name|=gsub(" "; "_") | [.id, .name] | join("%")'
+    curl -X GET -su "${bamboo_user}:${BAMBOO_PASSWORD}" \
+    "${bamboo_url}/rest/api/latest/agent/remote" \
+    | jq -r '.[] | .name|=gsub(" "; "_") | [.id, .name] | join("%")'
 )
 
 declare -a target_agent_ids=()
 
 for line in $(echo "$remote_agents" | grep "$host_pattern")
 do
-	agent_id="$(echo "$line" | cut -d% -f 1)"
-	agent_name="$(echo "$line" | cut -d% -f 2)"
-	echo "Agent '${agent_name}' matches (id '${agent_id}')"
-	target_agent_ids+=("${agent_id}")
+    agent_id="$(echo "$line" | cut -d% -f 1)"
+    agent_name="$(echo "$line" | cut -d% -f 2)"
+    echo "Agent '${agent_name}' matches (id '${agent_id}')"
+    target_agent_ids+=("${agent_id}")
 done
 
 if [ ${#target_agent_ids[@]} -eq 0 ]
 then
-	echo "No matching agents found."
-	exit 0
+    echo "No matching agents found."
+    exit 0
 fi
 
 echo "About to disable ${#target_agent_ids[@]} remote agents"
 
 for agent_id in "${target_agent_ids[@]}"
 do
-	curl -H 'Accept: application/json' -X PUT -su "${bamboo_user}:${BAMBOO_PASSWORD}" \
-	"${bamboo_url}/rest/api/latest/agent/${agent_id}/disable" | jq -M .
+    curl -H 'Accept: application/json' -X PUT -su "${bamboo_user}:${BAMBOO_PASSWORD}" \
+    "${bamboo_url}/rest/api/latest/agent/${agent_id}/disable" | jq -M .
 done
 
 echo 'Done'
